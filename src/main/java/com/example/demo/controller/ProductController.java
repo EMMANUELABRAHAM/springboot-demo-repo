7501e24a08cc7459fcd5b6c29b72badb73a625c3
emmanuelabraham <emmanuelabraham999@gmail.com>
2024-02-20 21:36:26 +0530

    Controller updated with file uploading to FileSystem

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,9 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.ProductUploadResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -16,5 +25,48 @@ public class ProductController {
     @PostMapping("/check/post")
     public String checkPostApi(@RequestParam("msg") String msg){
         return "Hello from POST API!" + msg;
+    }
+
+    //for uploading the SINGLE file to the File System
+    @PostMapping("/single/file")
+    public ResponseEntity<ProductUploadResponse> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        try {
+            file.transferTo(new File("/Users/emmanuelabraham/Documents/2024/Backend Projects/Spring Boot Demo/demo/Sample Data Storage/" + fileName));
+            String downloadUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/download/")
+                    .path(fileName)
+                    .toUriString();
+            ProductUploadResponse response = new ProductUploadResponse(fileName,
+                    downloadUrl,
+                    file.getContentType(),
+                    file.getSize());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    //for uploading the MULTIPLE file to the File system
+    @PostMapping("/multiple/file")
+    public ResponseEntity<List<ProductUploadResponse>> handleMultipleFilesUpload(@RequestParam("files") MultipartFile[] files) {
+        List<ProductUploadResponse> responseList = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String fileName = file.getOriginalFilename();
+            try {
+                file.transferTo(new File("/Users/emmanuelabraham/Documents/2024/Backend Projects/Spring Boot Demo/demo/Sample Data Storage/" + fileName));
+                String downloadUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                        .path("/download/")
+                        .path(fileName)
+                        .toUriString();
+                ProductUploadResponse response = new ProductUploadResponse(fileName,
+                        downloadUrl,
+                        file.getContentType(),
+                        file.getSize());
+                responseList.add(response);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        }
+        return ResponseEntity.ok(responseList);
     }
 }
